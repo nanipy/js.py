056d4e43c3624ead5c46f1b86ccfe350d1d3a964
Jens Reidel <38864617+Gelbpunkt@users.noreply.github.com>
2018-11-24 12:36:39 +0100

    Small fix

--- js/jslist.py
+++ js/jslist.py
@@ -13,7 +13,7 @@ class JSList(list):
 
     @length.setter
     def length(self, new_length):
-        if length < 0:
+        if new_length < 0:
             raise ArgumentError("New Array length must be positive")
         self = self[:new_length]
 
