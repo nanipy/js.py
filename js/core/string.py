fc0f28892d0fb1174d240b1ab98e331b700f69f3
Jens Reidel <38864617+Gelbpunkt@users.noreply.github.com>
2018-11-25 11:56:15 +0100

    Update string.py

--- js/core/string.py
+++ js/core/string.py
@@ -67,7 +67,7 @@ class String(str):
         raise NotImplementedError("To be implemented soon")
 
     def match(self, regexpr):
-        return Array(re.findall(regexpr, self))
+        return Array([String(i) for i in re.findall(regexpr, self)])
 
     def repeat(self, times):
         times = round(times)
